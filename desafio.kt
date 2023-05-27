0fda5ee4561572a014c046f4252f483df8635452
eric.silva <102669530+EricSilvaDev@users.noreply.github.com>
2023-05-27 17:43:05 -0300

    Segundo Commit

--- desafio.kt
+++ desafio.kt
@@ -2,25 +2,43 @@
 
 
 
+
+
 enum class Nivel { JUNIOR, PLENO, SENIOR, ESPECIALISTA }
 
-data class Desenvolvedor(val nome: String, val cpf: String, val idade: Int) {
+data class Desenvolvedor(val nome: String, val cpf: String, val idade: Int) 
     
-}
-
 data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
+data class Formacao(val nome: String, val conteudos: MutableSet<ConteudoEducacional>, val nivel: Nivel) {
 
-    val inscritos = mutableListOf<Desenvolvedor>()
+    val inscritos = mutableSetOf<Desenvolvedor>()
     
     fun matricular(desenvolvedor: Desenvolvedor): Unit {
 //         TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
-		inscritos.add(desenvolvedor)
+		val contains = inscritos.any { it == desenvolvedor }
+        
+        if(contains) {
+            println("O dev ${desenvolvedor.nome} ja esta matriculado nessa formacao")
+        }
+        else {
+            inscritos.add(desenvolvedor)
+            println("Novo dev ${desenvolvedor.nome} matriculado com sucesso!!!")
+        }
     }
     
     fun cancelarMatricula(desenvolvedor: Desenvolvedor): Unit {
-        inscritos.remove(desenvolvedor)
+        val contains = inscritos.any { it == desenvolvedor }
+        
+        if(contains){
+            val nome = desenvolvedor.nome;
+            inscritos.remove(desenvolvedor)
+            println("A matricula do dev $nome foi cancelada com sucesso")
+        }
+        
+        else{
+            println("O dev nao esta matriculado nessa formacao") 
+        }
     }
     
     fun listarInscritos() {
@@ -38,19 +56,23 @@ fun main() {
     val conteudoEducacional1 = ConteudoEducacional("Lógica de Programação",20)
     val conteudoEducacional2 = ConteudoEducacional("Programando em Kotlin",40)
     val conteudoEducacional3 = ConteudoEducacional("Banco de Dados MySQL",10)
-    val conteudoEducacional4 = ConteudoEducacional("Introdução ao Spring Framework",30)
     
-    var conteudos = mutableListOf<ConteudoEducacional>(conteudoEducacional1, conteudoEducacional2, conteudoEducacional3, conteudoEducacional4)
+    
+    var conteudos = mutableSetOf<ConteudoEducacional>(conteudoEducacional1, conteudoEducacional2, conteudoEducacional3)
     
     val formacao = Formacao("Kotlin Backend para Iniciantes", conteudos, Nivel.JUNIOR)
     
     formacao.matricular(dev1)
     formacao.matricular(dev2)
+    formacao.matricular(dev1)
     
     formacao.listarInscritos()
     
     formacao.cancelarMatricula(dev1)
     formacao.listarInscritos()
-
+	
+    val conteudoEducacional4 = ConteudoEducacional("Introdução ao Spring Framework",30)
+    formacao.conteudos.add(conteudoEducacional4)
+    print(formacao.conteudos)
     
 }
